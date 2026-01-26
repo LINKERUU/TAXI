9e195b9bf68557502d72b873dde410a986af9f34
dima <safonovdima330>
2026-01-27 00:03:49 +0300

    build: fix dependency conflicts and add discovery service configurations

--- trip-service/src/main/java/com/tripservice/client/DriverClient.java
+++ trip-service/src/main/java/com/tripservice/client/DriverClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "driver-service", url = "${driver.service.url}")
+@FeignClient(name = "driver-service")
 public interface DriverClient {
 
   @GetMapping("/api/drivers/{driverId}")
--- trip-service/src/main/java/com/tripservice/client/PassengerClient.java
+++ trip-service/src/main/java/com/tripservice/client/PassengerClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "passenger-service", url = "${passenger.service.url}")
+@FeignClient(name = "passenger-service")
 public interface PassengerClient {
 
   @GetMapping("/api/passengers/{passengerId}")
