f7fce96261411d45cec17e13c0e9095aff228b5c
dima <safonovdima330>
2026-02-01 21:16:17 +0300

    feat(grpc): add protobuf definitions and common configuration

--- trip-service/src/main/java/com/tripservice/client/PassengerClient.java
+++ trip-service/src/main/java/com/tripservice/client/PassengerClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "passenger-service")
-public interface PassengerClient {
+public interface  PassengerClient {
 
   @GetMapping("/api/passengers/{passengerId}")
   PassengerResponse getPassengerById(@PathVariable("passengerId") Long passengerId);
